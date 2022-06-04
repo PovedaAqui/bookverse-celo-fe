e31c736b250468dc71744128546620e0f9a1988b
luis.poveda93 <luis.poveda9321@gmail.com>
2022-06-05 00:12:31 +0200

    implementing mui

--- src/App.js
+++ src/App.js
@@ -1,111 +1,32 @@
 import React, { useEffect, useState } from 'react';
-//import { Metadata } from './data/Metadata';
+import ActionAreaCard from './components/ActionAreaCard';
+import SearchAppBar from './components/SearchAppBar';
 
-const Meta = [
-  {
-    "name": "CeloPunks Celo Connect Edition #377",
-    "image": "https://ipfs.io/ipfs/QmRX4tFHKajU9nAxwHqJjkwtvBFCNRwJXNzdqrXUA16o3Z/377.png",
-    "edition": 377
-    // // "dna": "2cf8d6bd427cbbc6b544873c47ad45c549f295d7",
-    // // "description": "Special CeloPunks Edition created for the Workshop at the Celo Connect to celebrate the presence of CeloPunks at the Barcelona conference. This edition live in 500 pieces on the Celo Blockchain and is free to mint. CeloPunks are frens of Celo but we are not affiliated neither with them nor with LarvaLabs.",
-    // // "date": 1648755343525,
-    // // "compiler": "HashLips Art Engine",
-    // // "attributes": [
-    // //   {
-    // //     "value": "Celo Connect Dot Pattern ",
-    // //     "trait_type": "Background"
-    // //   },
-    // //   {
-    // //     "value": "Blue Woman ",
-    // //     "trait_type": "Character"
-    // //   },
-    // //   {
-    // //     "value": "EyeMask ",
-    // //     "trait_type": "Eyes"
-    // //   },
-    // //   {
-    // //     "value": "Ushanka ",
-    // //     "trait_type": "Head"
-    // //   },
-    // //   {
-    // //     "value": "Passion Red Lipstick ",
-    // //     "trait_type": "Lip"
-    // //   }
-    // ]
+export const App = () => {
+
+  const AppBar = () => {
+
+    const apptitle = 'Bookverse';
+
+    return(
+      <div>
+       <SearchAppBar apptitle={apptitle} color='primary' variant='contained' text='Connect Wallet' handle={()=>console.log('works')}/>
+      </div>
+    )
+  };
+
+  const Card = () => {
+
+    return(
+      <div>
+        <ActionAreaCard description='Test'/>
+      </div>
+    )
   }
-  ];
-
-export const DefaultContainer = () => {
 
   return(
     <div>
-      <Container1 />
+      <AppBar />
     </div>
-  )
+  );
 };
-
-const Container1 = () => {
-  const [search, setSearch] = useState('');
-  const [metadata, setMetadata] = useState('');
-
-    useEffect(()=>{
-    fetch(`https://api-eu1.tatum.io/v3/nft/address/balance/ETH/0x1070F9e5eDD7d77a2817bd71512Ec4Ede358105b`,
-    {
-      method: 'GET',
-      headers: {
-        'x-api-key': '5ff7b3e5-f465-4cc2-b887-fa0f2baf2e5b'
-      }
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then((data) => {
-        return (
-          <div>
-          {setMetadata(data)}
-          </div>
-          );
-      });
-  }, []);
-
-  return(
-  <div>
-    <form>
-      <input 
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        required
-      />
-    </form>
-    <Container2 data = {metadata}/>
-  </div>
-  )
-};
-
-const Container2 = (props) => {
-  const data = Array.from(props.data);
-
-  return(
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => {
-            return item.metadata.map(item2 => {
-              return(
-                <tr key={data.id}>
-                  <td>{item2.metadata.name}</td>
-                </tr>
-              )
-            })
-          })}
-        </tbody>
-      </table>
-    </div>
-  )
-}
