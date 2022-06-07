6b40c6a9f752a177a29d8d426498859dcfbda9f0
luis.poveda93 <luis.poveda9321@gmail.com>
2022-06-07 22:27:25 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,74 +1,21 @@
-import { Button } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import ActionAreaCard from './components/ActionAreaCard';
-import SearchAppBar from './components/SearchAppBar';
-import {
-  connectWallet,
-  getCurrentWalletConnected,
-  disconnectWallet
-} from "./util/interact.js";
+import { NavBar } from './pages/NavBar';
+import MyBooks from './pages/MyBooks';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+const App = () => {
 
-export const App = () => {
-
-  const [walletAddress, setWallet] = useState("");
-  const apptitle = 'Bookverse';
-
-  useEffect(async() => {
-
-    const { address } = await getCurrentWalletConnected();
-
-    setWallet(address);
-
-    addWalletListener();
-  }, []);
-
-  function addWalletListener() {
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        if (accounts.length > 0) {
-          setWallet(accounts[0]);
-          //setStatus("👆🏽 Write a message in the text-field above.");
-        } else {
-          setWallet("");
-        }
-      });
-    } else {
-      console.log("error");
-    }
-  }
-  
-  const connectWalletPressed = async () => {
-    const walletResponse = await connectWallet(); 
-    setWallet(walletResponse.address);
-  };
-
-  const connectWalletChecker = () => {
-
-    return(
-      <div>
-        <SearchAppBar id='walletButton' apptitle={apptitle} color='primary' variant='contained' text='Connect Wallet' handle={connectWalletPressed} startIcon={null}/>
-      </div>
+    return (
+        <Router>
+            <div>
+                <NavBar />
+                <div>
+                    <Routes>
+                        <Route path='/' element={<MyBooks />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
     )
-  }
+}
 
-  const WalletAddressSent = (
-    walletAddress.substring(0, 5) + '…' + walletAddress.substring(walletAddress.length - 4)
-  );
-
-  const AlreadyConnected = () => {
-
-    return(
-      <div>
-        <SearchAppBar id='walletButton' apptitle={apptitle} color='secondary' variant='text' text={WalletAddressSent} handle={disconnectWallet} colorIcon='error'/>
-      </div>
-    )
-  }
-
-  return(
-    <div>
-      {walletAddress ? AlreadyConnected() : connectWalletChecker()}
-      
-    </div>
-  )
-};
+export default App;
