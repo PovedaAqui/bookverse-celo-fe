4bfda1e7e57e629b54ab63e18d6808716e54737b
luis.poveda93 <luis.poveda9321@gmail.com>
2022-06-21 20:18:50 +0200

    Circular progress added

--- src/components/ActionAreaCard.js
+++ src/components/ActionAreaCard.js
@@ -3,20 +3,28 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import CircularProgress from './CircularProgress';
 
 export default function ActionAreaCard({...props}) {
 
   const { name, description, image } = props;
+  let url = "";
+
+  if(image!==null) {
+    url = image.replace("ipfs://", "https://ipfs.io/ipfs/")
+  } else {
+    return image;
+  }
 
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea onClick={()=>console.log("TEST")}>
-        <CardMedia
+        {url===null ? <CircularProgress/> : <CardMedia
           component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
+          height="200"
+          image={url}
+          alt="jpeg"
+        />}
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {name}
