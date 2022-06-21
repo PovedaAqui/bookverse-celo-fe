552cce7852fdb3d3409701ad562b0a8f82f79dc3
luis.poveda93 <luis.poveda9321@gmail.com>
2022-06-22 00:03:58 +0200

    adding grid

--- src/components/ActionAreaCard.js
+++ src/components/ActionAreaCard.js
@@ -2,8 +2,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { CardActionArea } from '@mui/material';
 import CircularProgress from './CircularProgress';
+import Grid from '@mui/material/Grid';
 
 export default function ActionAreaCard({...props}) {
 
@@ -17,28 +18,27 @@ export default function ActionAreaCard({...props}) {
   }
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea onClick={()=>console.log("TEST")}>
-        {url===null ? <CircularProgress/> : <CardMedia
-          component="img"
-          height="200"
-          image={url}
-          alt="jpeg"
-        />}
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Sell
-        </Button>
-      </CardActions>
-    </Card>
+    <Grid container>
+      <Grid item xs={12} sm={4}>
+        <Card sx={{ maxWidth: 345 , mt: 3}}>
+          <CardActionArea onClick={()=>console.log("TEST")}>
+            {url===null ? <CircularProgress/> : <CardMedia
+              component="img"
+              height="200"
+              image={url}
+              alt="jpeg"
+            />}
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
