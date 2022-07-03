04b22a8958c3797c1a0ecf1c2aed18bad6fe9375
luis.poveda93 <luis.poveda9321@gmail.com>
2022-07-03 03:25:05 +0200

    page updated

--- src/pages/SellingNFT.js
+++ src/pages/SellingNFT.js
@@ -4,10 +4,13 @@ import { useState, useEffect } from "react";
 const SellingNFT = () => {
 
     const [initiated, setInitiated] = useState([]);
-    const [listingId, setListingId] = useState(null);
+    const [listingId, setListingId] = useState([]);
+    const [metadata, setMetadata] = useState([]);
     
 
     const initialURL = `http://localhost:3001/api/getListing`;
+    const getInitiatedURL = `http://localhost:3001/api/getInitiated`;
+    const metadataURL = `http://localhost:3001/api/getMetadata`;
 
     useEffect(() => {
         fetch(initialURL,
@@ -23,27 +26,50 @@ const SellingNFT = () => {
             .then(data => setListingId((data.data).map(data2 => {
                 return data2;
             })))
-        listingId!==null && getInitiated();
     }, [])
 
-    const getInitiatedURL = `http://localhost:3001/api/getInitiated`;
+    //console.log(listingId);
 
-    const getInitiated = () => {
-        listingId.map(data => {
-            fetch(getInitiatedURL,
-                {
-                    method: 'POST',
-                    mode: 'cors',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({id: data})
-                })
-            .then(response => response.json())
-            .then(data => setInitiated(data.data))
-    })}
+    useEffect(() => {
+        const getInitiated = () => {
+            listingId.map(data => {
+                fetch(getInitiatedURL,
+                    {
+                        method: 'POST',
+                        mode: 'cors',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({id: data})
+                    })
+                .then(response => response.json())
+                .then(data => setInitiated(data.data))
+        })}
+        listingId!==null && listingId.length>0 && getInitiated()
+    }, [listingId])
+
+    //console.log(initiated);
+
+    useEffect(() => {
+       
+            const {nftAddress, tokenId} = initiated;
+            const getMetadata = () => {    
+                fetch(metadataURL,
+                    {
+                        method: 'POST',
+                        mode: 'cors',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({nftAddress: nftAddress, tokenId: tokenId})
+                    })
+                .then(response => response.json())
+                .then(data => setMetadata(data.data))
+            }
+            tokenId!==undefined && nftAddress!==undefined && getMetadata();             
+    }, [initiated])
     
-    console.log(initiated);
+    console.log(metadata);
 
     return (
         <div>
