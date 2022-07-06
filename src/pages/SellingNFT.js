9f3ae4ba01d6b03561e488a7e9198f952024c825
luis.poveda93 <luis.poveda9321@gmail.com>
2022-07-07 01:41:39 +0200

    pending to improve render

--- src/pages/SellingNFT.js
+++ src/pages/SellingNFT.js
@@ -56,7 +56,7 @@ const SellingNFT = ({address}) => {
                     })
                 .then(response => response.json())
                 .then(data => arrayInitiated.push(data.data))
-                .then(data2 => data2>=listingId.length && setInitiated(arrayInitiated))
+                .then(data2 => data2===listingId.length && setInitiated(arrayInitiated))
                 .then(setFetchInitiated(false))
         })}
         fetchInitiated && listingId!==null && listingId.length>0 && initiated.length==0 && getInitiated()
@@ -79,8 +79,8 @@ const SellingNFT = ({address}) => {
                         })
                     .then(response => response.json())
                     .then(data => arrayMetadata.push(data.data))
-                    .then(data2 => data2>=initiated.length && arrayMetadata.map(data => data.data.replace("ipfs://", "https://ipfs.io/ipfs/")))
-                    .then(data3 => data3.length>=initiated.length && setMetadata(data3))
+                    .then(data2 => data2===initiated.length && arrayMetadata.map(data => data.data.replace("ipfs://", "https://ipfs.io/ipfs/")))
+                    .then(data3 => data3.length===initiated.length && setMetadata(data3))
                     .then(setFetchMetadata(false))                  
                 })}
                 fetchMetadata && initiated!==null && initiated.length>=listingId.length && getMetadata()
@@ -95,7 +95,7 @@ const SellingNFT = ({address}) => {
                 fetch(data)
                 .then(response => response.json())
                 .then(data => arrayIPFS.push(data))
-                .then(data4 => data4>=metadata.length && setIPFS(arrayIPFS))
+                .then(data4 => data4===metadata.length && setIPFS(arrayIPFS))
                 .then(setFetchIPFS(false))                  
             })}
             fetchIPFS && metadata!==null && metadata.length>0 && getIPFS()
@@ -106,16 +106,17 @@ const SellingNFT = ({address}) => {
     return (
         <div>
             <Grid container>
-                {metadata!=undefined && metadata!=null && ipfs.map(({name, description, image}, i1)=>
-                    {return initiated.map(({tokenId, seller, price}, i2)=>{
-                        if(i1==i2){
+                {ipfs.length===metadata.length && ipfs.map(({name, description, image}, i1)=>
+                {
+                    return initiated.map(({tokenId, seller, price}, i2)=>{
+                        if(i1===i2){
                             return (
                                 <div key={i2}>
                                     {<Grid item xs={6} md={4} key={name}><ActionAreaCard name={name} description={description} image={image} tokenId={tokenId} address={address} seller={seller} listedPrice={price} listingId={(name + tokenId).replace(/ /g, '')}/></Grid>}
                                 </div>
                             )
                         }
-                    })               
+                    })                 
                 })}
             </Grid>
         </div>
